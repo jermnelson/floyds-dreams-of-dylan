67500333e62de1a5ea56b99baa73355402233fbc
Jeremy Nelson <jermnelson@gmail.com>
2016-07-18 16:49:40 -0600

    added three podcasts

--- project/app.py
+++ project/app.py
@@ -1,3 +1,6 @@
+import os
+import rdflib
+
 from flask import Flask
 from flask_flatpages import FlatPages
 from flask_frozen import Freezer
@@ -6,3 +9,19 @@ app = Flask(__name__)
 app.config.from_pyfile('settings.py')
 pages = FlatPages(app)
 freezer = Freezer(app)
+
+global PODCASTS
+
+def setup():
+    PODCASTS = rdflib.Graph()
+
+    PROJECT_DIR = os.path.split(os.path.abspath(__file__))[0]
+    walker = os.walk(os.path.join(PROJECT_DIR, "podcasts"))
+    for result in walker:
+        print("IN ROW")
+        for name in result[2]:
+            if name.endswith("ttl"):
+                filepath  = "{}/{}".format(result[0], name)
+                print(filepath)
+                PODCASTS.parse(filepath, format='turtle')
+setup()
--- project/views.py
+++ project/views.py
@@ -1,9 +1,14 @@
+__author__ = "Jeremy Nelson"
+
+import os
+import rdflib
 from flask import render_template
-from .app import app, pages
+from .app import app, pages, PODCASTS
+
 
 @app.route("/")
 def home():
-    return render_template("index.html", pages=pages)
+    return render_template("index.html", pages=pages, podcasts=PODCASTS)
 
 @app.route("/<path:path>/")
 def display(path):
