27190780155fafc41d01fe34dd0b7c8c19b02e97
Jeremy Nelson <jermnelson.gmail.com>
2020-01-16 16:06:19 -0700

    Added three concert setlists for Pirate radio

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from project.app import app
 from project.views import *
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8002, host='0.0.0.0')
+    app.run(debug=True, port=8003, host='0.0.0.0')
