5de7cba3222200c76a2a479eff9597ddf54c21c8
Jeremy Nelson <jermnelson@gmail.com>
2020-01-26 11:38:06 -0700

    Changed to run on port 80 as the default web app

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
+
+#!/home/pi/py3-env/bin/python
 from project.app import app
 from project.views import *
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8003, host='0.0.0.0')
+    app.run(debug=True, port=80, host='0.0.0.0')
