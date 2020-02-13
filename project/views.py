52b358caa1771bbeef8c75d9dc86bea614a78177
Jeremy Nelson <jermnelson@gmail.com>
2020-02-12 20:42:32 -0700

    Updated with text and images

--- project/views.py
+++ project/views.py
@@ -11,6 +11,14 @@ def home():
     podcats = []
     return render_template("index.html", pages=pages)
 
+@app.route('/books/<path:title>/')
+def moby_dick(title):
+    if title.lower().startswith("moby-dick"):
+        template = 'MobyDickOrtheWhalebyHermanMelville.html'
+    elif title.lower().startswith("the-odyssey"):
+        template = "TheOdysseybyHomer.html"
+    return render_template(template)
+
 @app.route("/<path:path>/")
 def display(path):
     page = pages.get_or_404(path)
